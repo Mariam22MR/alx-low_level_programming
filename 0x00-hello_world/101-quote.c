c6900c25e56c04ab8e9463525d59c107f86781b0
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-08-11 05:41:41 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,13 @@
-#incleude <studeo.h>
-incleude <studei.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print with printf and puts function
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
