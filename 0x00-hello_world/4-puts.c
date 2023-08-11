c56f2ca9574f6cd4adc18835b7c35bdcca2263af
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-08-11 05:07:43 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#includde <stdio.h>
+#include <stdio.h>
+
 /**
+ * main - prints with puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
