d288f5b6325c9836d48855ad7bf1e8de5c33ddd0
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-08-16 03:29:25 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,22 @@
-#bin
+#include "main.h"
+
+/**
+ * jack_bauer - prints the all time of a day
+ */
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
