bd48a58478d5eefc2c6794db0c478dc72f364b39
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-08-16 03:32:07 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,17 @@
-bin
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: an integer variable
+ *
+ * @b: an integer variable
+ *
+ * Return: returns the result
+ */
+int add(int a, int b)
+{
+	int res = a + b;
+
+	return (res);
+}
