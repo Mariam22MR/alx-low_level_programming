1ff6fc7bef1a7134cea8f4934d7377384bb24fe8
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-08-18 04:49:14 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,1 +1,37 @@
-ivlod
+#include <stdio.h>
+#include <math.h>
+
+
+/**
+ * main - find the prime factors
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	long int a = 612852475143;
+	long int b = -1;
+	int c;
+
+	while (a % 2 == 0)
+	{
+		b = 2;
+		a = a / 2;
+	}
+
+	for (c = 3; c <= sqrt(a); c = c + 2)
+	{
+		while (a % c == 0)
+		{
+			b = c;
+			a = a / c;
+		}
+	}
+	if (a > 2)
+		b = a;
+	printf("%ld\n", b);
+
+	return (0);
+}
