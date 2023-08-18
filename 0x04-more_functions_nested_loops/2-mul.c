2e37f63d3aec2c576eaa07103b889de347f5ae54
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-08-18 04:00:42 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * mul - multiplies tow integers
+ * @a: to check the first integer
+ * @b: to check the scanod integer
+ * Return: mult
  */
-int main(void)
+
+int mul(int a, int b)
 {
-	printf("%d\n", mul(98, 1024));
-	printf("%d\n", mul(-402, 4096));
-	return (0);
+	int mult;
+
+	mult = (a * b);
+
+	return (mult);
 }
