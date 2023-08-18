ee9269cef2f4d0776ae340d3bb7354dcfe508c41
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-08-18 04:13:27 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,15 @@
-iclode
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints a numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
