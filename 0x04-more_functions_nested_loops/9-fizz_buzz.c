a9d3d19545c45e5ef6004b66764887fe155a94b9
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-08-18 04:36:36 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,31 @@
-iclod
+#include <stdio.h>
+
+/**
+ * main - print while multiples of 3 and 5 and 15
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 1;
+
+	while (a <= 100)
+	{
+		if (a == 100)
+			printf("Buzz");
+		else if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz ");
+		else if (a % 3 == 0 && a % 5 != 0)
+			printf("Fizz ");
+		else if (a % 5 == 0 && a % 3 != 0)
+			printf("Buzz ");
+		else
+			printf("%d ", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
