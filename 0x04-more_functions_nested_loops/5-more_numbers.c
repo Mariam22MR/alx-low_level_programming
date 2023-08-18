14ad5b009ca388450626467c77d8fcb8bf3620d4
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-08-18 04:25:04 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,24 @@
-iclod
+#include "main.h"
+
+/**
+ * more_numbers - print 10 time the numbers fro 0 to 14
+ * follower by new line
+ */
+
+void more_numbers(void)
+{
+	int num, count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
