c816be0a4c8a9da69274049f456c5f10fc40c1fd
Mariam22MR <Xxxsaakk@gmail.com>
2023-08-23 22:48:38 -0700

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; src[i] != '\0'; i++, ++len)
+	{
+		dest[len] = src[i];
+	}
+	dest[len] = '\0';
+
+	return (dest);
+}
