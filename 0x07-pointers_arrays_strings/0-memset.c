47b76f0918a31f035c46e0f3e2489c77376bc739
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-08-29 04:44:48 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,9 +5,9 @@
  * @s: pointer to put the constant
  * @b: char to copy
  * @n: max bytes to use
- *
  * Return: pointer s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int j;
