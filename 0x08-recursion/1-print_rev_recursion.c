8043d6dac95c4aa780838eea426e0665fff4a50c
Mariam22MR <Xxxsaakk@gmail.com>
2023-08-30 13:42:29 -0700

    a

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - prints a reversed str
+ *@s: pointer block of memory to fill
+ *Return: always 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
