b8c5b632f952bf747d5c1c9b6164132f12b69d20
Mariam22MR <Xxxsaakk@gmail.com>
2023-09-03 12:31:51 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 	{}
 
-	for (c = 0; src[j] != '\0' && c < n; c++)
+	for (c = 0; src[i] != '\0' && c < n; c++)
 	{
 		dest[i + c] = src[c];
 	}
