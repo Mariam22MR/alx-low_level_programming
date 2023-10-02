e6e018752286182c50768eaaa3a040e5b80d3726
Mariam22MR <xxxsaakk@gmail.com>
2023-10-02 14:58:33 -0700

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
-* create_file - creates a file.
-* @filename: name of file to creat.
+* append_text_to_file - appends text to the file.
+* @filename: name of the file to create.
 * @text_content: content of file.
 *
 * Return: 1 on success, 0 on failure.
 */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, i;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
