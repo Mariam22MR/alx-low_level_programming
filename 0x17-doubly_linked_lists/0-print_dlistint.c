0ebd243fd9903999db5b6c36675405f2168b879f
Mariam22MR <Xxxsaakk@gmail.com>
2023-12-08 20:33:47 -0800

    a

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,7 +6,7 @@
  * Return: Size of list
  */
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t x = 0;
 	while (h)
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,7 +6,7 @@
  * Return: Size of list
  */
 
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t x = 0;
 	while (h)
