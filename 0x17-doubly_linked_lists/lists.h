8b180f9bd71af3f7dcfab38751253d3e488c05b1
Mariam22MR <Xxxsaakk@gmail.com>
2023-12-08 21:02:48 -0800

    a

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -19,5 +20,6 @@
 	struct dlistint_s *next;
 } dlistint_t;
 
-size_t print_dlistint(const dlistint_t *h)
-size_t dlistint_len(const dlistint_t *h)
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+#endif
