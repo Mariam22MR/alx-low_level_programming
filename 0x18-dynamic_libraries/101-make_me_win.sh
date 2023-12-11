dc8ced53fe30f9fb1ab487fc87261f5c2016d5db
Mariam22MR <Xxxsaakk@gmail.com>
2023-12-11 14:25:40 -0800

    a

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Mariam22MR/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -p /tmp https://github.com/Mariam22MR/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
