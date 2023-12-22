11d1c0fcecc4588593ed6e8f3bd4b1d095040eca
Mariam22MR <xxxsaakk@gmail.com>
2023-12-22 13:02:17 -0800

    a

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,22 +10,23 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int n = 0;
-	hash_node_t *node;
+	hash_node_t *next;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0)
 		return;
 
-	for (n < ht->size; n++)
+	while (n < ht->size)
 	{
 		while (ht->array[n] != NULL)
 		{
-			node = ht->array[n]->node;
+			next = ht->array[n]->next;
 			free(ht->array[n]->key);
 			free(ht->array[n]->value);
 			free(ht->array[n]);
 			ht->array[n] = next;
 		}
 	}
+	n++
 	free(ht->array);
 	ht->array = NULL;
 	ht->size = 0;
