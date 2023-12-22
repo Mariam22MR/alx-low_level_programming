3a48670311d30f77cf100015d2461cf93968ba67
Mariam22MR <xxxsaakk@gmail.com>
2023-12-22 07:21:06 -0800

    a

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,10 +15,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
-
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
+		free(hash_table);
 		return (NULL);
 	for (n = 0; n < size; n++)
 		hash_table->array[n] = NULL;
