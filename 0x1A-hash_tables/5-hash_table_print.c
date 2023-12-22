3932cd5a305b19314945577340a28b3ea9d5aef0
Mariam22MR <xxxsaakk@gmail.com>
2023-12-22 13:57:08 -0800

    a

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,27 +11,22 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *tmp;
 	unsigned long int n;
-	unsigned char flag = 0;
+	char flag = 0;
 
-	if (ht == NULL)
+	if (ht == NULL || ht->array == NULL)
 		return;
-
 	printf("{");
 	for (n = 0; n < ht->size; n++)
 	{
-		if (ht->array[n] != NULL)
+		tmp = ht->array[i];
+		while (tmp != NULL)
 		{
 			if (flag == 1)
 				printf(", ");
-
-			tmp = ht->array[n];
-			while (tmp != NULL)
-			{
-				printf("'%s': '%s'", tmp->key, tmp->value);
-				flag = 1;
-				tmp = tmp->next;
-			}
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			flag = 1;
+			tmp = tmp->next;
 		}
 	}
 	printf("}\n");
-
+}
