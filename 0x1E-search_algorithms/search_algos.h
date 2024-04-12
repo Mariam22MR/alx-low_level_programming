8a68ece81d53da093e4e9599258268f8990b9686
Mariam22MR <xxxsaakk@gmail.com>
2024-04-11 19:42:12 -0700

    a

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,16 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_arr(int *array, size_t s, size_t e);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+#endif
