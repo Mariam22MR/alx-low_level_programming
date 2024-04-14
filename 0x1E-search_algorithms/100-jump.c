78a3d829a5aac8861e93021c7199fc9c45034abf
Mariam22MR <xxxsaakk@gmail.com>
2024-04-14 15:38:35 -0700

    a

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -7,9 +7,8 @@
  * @value: Value being searched for.
  *
  * Return: If value is not present or array is NULL, -1.
- * 			first index where value is located.
+ *
  * Description: Prints value every time it is compared in array.
- *              Uses square root of array size as jump step.
  */
 int jump_search(int *array, size_t size, int value)
 {
